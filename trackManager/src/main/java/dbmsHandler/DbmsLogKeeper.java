57a2838d08f803b6a73b46fe2bc9fbaea2784847
Amrit Raj <amritrajnits@gmail.com>
2020-06-03 13:14:40 +0530

    Updated as per Code review 1.

--- trackManager/src/main/java/dbmsHandler/DbmsLogKeeper.java
+++ trackManager/src/main/java/dbmsHandler/DbmsLogKeeper.java
@@ -1,6 +1,6 @@
 package dbmsHandler;
 /*
-	Code:- Amrit Raj
+	@Amrit Raj
  	This class is exclusively for keeping the logs generated out of
  	trackManager.java file. This stores and shows the logs which 
  	come up during the execution of the saild code via a lambda 
@@ -11,17 +11,19 @@ import java.util.logging.Logger;
 public class DbmsLogKeeper{
 	//Our Logger instance..
 	private static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-	//Constructor
-	public DbmsLogKeeper() {
-		//Nothing to do..
-	}
-	//Marking the beginning of the Logging segment..
-	public void markBegining(String s) {
+	//For making an info level log..
+	static public void makeInfoLog(String s) {
 		LOGGER.log(Level.INFO, s);
 	}
-	//For making logs other than the first Log..
-	public void makeLog(String s) {
-		LOGGER.log(Level.INFO, s);
+	
+	//For making an fine level log..
+	static public void makeFineLog(String s) {
+		LOGGER.log(Level.FINE, s);
+	}
+	
+	//For making an Severe level log..
+	static public void makeSevereLog(String s) {
+		LOGGER.log(Level.SEVERE, s);
 	}
 }
 //End of the code..
