90d2c81a24d6b7ff9bf759e4e769978b43620539
Amrit Raj <amritrajnits@gmail.com>
2020-06-11 14:24:04 +0530

    Updated to handle Tie situation

--- RecommendPlaylist/src/main/java/RecommendPlaylist/PlaylistSort.java
+++ RecommendPlaylist/src/main/java/RecommendPlaylist/PlaylistSort.java
@@ -2,20 +2,29 @@ package RecommendPlaylist;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Comparator;
+import java.util.ArrayList;
 public class PlaylistSort implements Comparator<String>{
-	private HashMap<String, Integer> frequency = new HashMap<String, Integer>();
-	public PlaylistSort(HashMap<String, Integer> arr) {
+	private HashMap<String, ArrayList<Integer>> frequency = new HashMap<String, ArrayList<Integer>>();
+	public PlaylistSort(HashMap<String, ArrayList<Integer>> arr) {
 		frequency = arr;
 	}
 	public int compare(String s1, String s2) {
-		if(frequency.get(s1) < frequency.get(s2)) {
+		if(frequency.get(s1).get(0) < frequency.get(s2).get(0)) {
 			return 1;
 		}
-		else if(frequency.get(s1) > frequency.get(s2)) {
+		else if(frequency.get(s1).get(0) > frequency.get(s2).get(0)) {
 			return -1;
 		}
 		else {
-			return 0;
+			if((frequency.get(s1).get(0) + frequency.get(s1).get(1)) < (frequency.get(s2).get(0) + frequency.get(s2).get(1))) {
+				return 1;
+			}
+			else if((frequency.get(s1).get(0) + frequency.get(s1).get(1)) > (frequency.get(s2).get(0) + frequency.get(s2).get(1))) {
+				return -1;
+			}
+			else {
+				return 0;
+			}
 		}
 	}
 }
