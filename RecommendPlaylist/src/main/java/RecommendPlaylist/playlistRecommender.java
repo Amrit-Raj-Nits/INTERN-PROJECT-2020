bd6f12565799de98acd419fcbe55f7884ae0d363
Amrit Raj <amritrajnits@gmail.com>
2020-06-12 14:57:02 +0530

    Removed Some metods and added to a new class

--- RecommendPlaylist/src/main/java/RecommendPlaylist/playlistRecommender.java
+++ RecommendPlaylist/src/main/java/RecommendPlaylist/playlistRecommender.java
@@ -1,4 +1,4 @@
-package RecommendPlaylist;
+package recommendplaylist;
 //Basic utilities import..
 import java.io.*;
 import java.util.*;
@@ -29,8 +29,9 @@ import com.amazonaws.services.dynamodbv2.model.PutItemResult;
 import com.amazonaws.services.dynamodbv2.model.ReturnConsumedCapacity;
 import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 
-public class playlistRecommender implements RequestHandler<Object, String> {
+public class PlaylistRecommender implements RequestHandler<Object, String> {
 	recommendPlaylistLogKeeper myLog = new recommendPlaylistLogKeeper();
+	RecommendPlaylistUtil rputil = new RecommendPlaylistUtil();
 	/*
 	 * Purpose - This is the main method that is called by the Lambda function on API trigger.
 	 * Arguments - 1. Event object obj consisting of asin and uid 2. Context
@@ -39,7 +40,7 @@ public class playlistRecommender implements RequestHandler<Object, String> {
 	public String handleRequest(Object obj, Context context) {
 		//Creating a ArrayList<String> of the asin at index 0 and uid at index 1..
 		ArrayList<String> credentials = new ArrayList<String>();
-		credentials = getCredentials(obj);
+		credentials = rputil.getCredentials(obj);
 		String asin = credentials.get(0);
 		String uid = credentials.get(1);
 		myLog.logInfo("Asin = "+asin+" Uid = "+uid);
@@ -57,35 +58,30 @@ public class playlistRecommender implements RequestHandler<Object, String> {
 		}
 
 		//Getting the track info from the database and storing it into the trackDetailsMap..
-		getTrackDetails(trackDetailsMap, dynamodb, asin);
+		fetchTrackDetails(trackDetailsMap, dynamodb, asin);
 		myLog.logInfo("Track Details = "+trackDetailsMap);
 		
 		//Getting the list of all the playlists with the given uid and storing into a playlistItems..
 		ArrayList<Item> playlistItems = new ArrayList<Item>();
-		getPlaylists(playlistItems, dynamodb, uid);
+		rputil.getPlaylists(playlistItems, dynamodb, uid);
+		
+		//An ArrayList of names of playlist..
+		ArrayList<String> names = new ArrayList<String>();
+		rputil.getNames(names, playlistItems);
+		
+		//Creating a map to store key = name of playlist and value = ArrayList<Integer> with 2 values each, at index 0 = score, 1 = tieBreakerScore..
+		HashMap<String, ArrayList<Integer>> playlistScoreMap = new HashMap<String, ArrayList<Integer>>();
 		
 		//Now we will generate the score for all the Items in the ArrayList ..the scores will correspond to each playlist index wise in playlistItems
 		ArrayList<Integer> scores = new ArrayList<Integer>();
 		ArrayList<Integer> tieBreakerScores = new ArrayList<Integer>();
-		generateScores(scores, tieBreakerScores, trackDetailsMap, playlistItems);
+		generateScores(playlistScoreMap, trackDetailsMap, playlistItems);
 		
-		//An ArrayList of names of playlist..
-		ArrayList<String> names = new ArrayList<String>();
-		getNames(names, playlistItems);
 		myLog.logInfo("Playlists in the given uid = "+names);
 		myLog.logInfo("Scores of corresponding Playlists = "+scores);
 		myLog.logInfo("Tie Breaker Scores of corresponding Playlists = "+tieBreakerScores);
 		
 		//Now we sort the playlistItems ArrayList based on scores ArrayList using a comparator..
-		//Creating a map to store key = name of playlist and value = ArrayList<Integer> with 2 values each, at index 0 = score, 1 = tieBreakerScore..
-		HashMap<String, ArrayList<Integer>> playlistScoreMap = new HashMap<String, ArrayList<Integer>>();
-		for(int i=0; i<names.size(); i++) {
-			ArrayList<Integer> mapFiller = new ArrayList<Integer>();
-			//Error debugging..
-			mapFiller.add(scores.get(i));
-			mapFiller.add(tieBreakerScores.get(i));
-			playlistScoreMap.put(names.get(i), mapFiller);
-		}
 		PlaylistSort sorter = new PlaylistSort(playlistScoreMap);
 		Collections.sort(names, sorter);
 		//Creating a String of playlist names in order for returning..
@@ -105,27 +101,14 @@ public class playlistRecommender implements RequestHandler<Object, String> {
 			return "Success--> You do not have any existing playlists, Kindly create one.";
 		}
 	}
-	/*
-	 * Purpose - To extract asin and uid from an Event Object.
-	 * Arguments - Event Object containing asin and uid
-	 * Returns - An ArrayList of size 2 with pasin at index 0 and uid at index 1.
-	 */
-	public ArrayList<String> getCredentials(Object obj){
-		ArrayList<String> cred = new ArrayList<String>(); 
-		String eventObject = obj.toString();
-		String tempAsin = eventObject.substring(eventObject.indexOf("=")+1, eventObject.indexOf(","));
-		String tempUid = eventObject.substring(eventObject.lastIndexOf("=")+1, eventObject.indexOf("}"));
-		cred.add(tempAsin);
-		cred.add(tempUid);
-		return cred;
-	}
+
 	/*
 	 * Purpose - To Get the track details of the track given by the asin of the track and store it into a given HashMap
 	 * Arguments - 1. A HashMap which will be populated 2. DynamoDB instance 3. A query Sting with asin
 	 * Returns - Nothing, rater populates the HashMap passed.
 	*/
-	public void getTrackDetails(HashMap<String, String> trackDetailsMap, DynamoDB dynamodb, String queryString) {
-		Table trackTable = dynamodb.getTable("track-repository");
+	public void fetchTrackDetails(HashMap<String, String> trackDetailsMap, DynamoDB dynamodb, String queryString) {
+		Table trackTable = dynamodb.getTable(Constants.TABLE_NAME1);
 		ItemCollection currentItem = trackTable.query("asin", queryString);
 		Iterator itr = currentItem.iterator();
 		
@@ -160,34 +143,11 @@ public class playlistRecommender implements RequestHandler<Object, String> {
 			}
 	}
 	/*
-	 * Purpose - To get all the playlists for a given uid and store it in a provided arraylist
-	 * Arguments - 1. An ArrayList to fill with playlists 2. DynamoDB instance 3. String containing the uid
-	 * Returns - void, populates the provided ArrayList
-	 */
-	public void getPlaylists(ArrayList<Item> playlistItems, DynamoDB dynamodb, String queryString) {
-		Table playlistTable = dynamodb.getTable("user-playlist-info");
-		ItemCollection currentItem = playlistTable.query("uid", Integer.parseInt(queryString));
-
-		Iterator itr = currentItem.iterator();
-		try{
-			//The Integer.parseInt() and Double.parseDouble() are vulnerable to NumberFormatException..
-			while(itr.hasNext()) {
-				//Logging the details from each item..
-				Item temp = (Item)itr.next();
-				playlistItems.add(temp);
-			}
-		}
-	  	catch(Exception e) {
-	  		System.out.println("Error is->"+e);
-			return;
-		}
-	}
-	/*
 	 * Purpose - To find the scores of a all playlists for a given uid and with respect to a given asin track
-	 * Arguments - 1. 2 X ArrayList<Integer> 2.HashMap<String, String> containing details of the track 3. ArrayList<Item> containing all playlists for a uid
-	 * Returns - void, populates the scores ArrayList<Integer>  
+	 * Arguments - 1. HashMap<String, ArrayList<Integer>> 2.HashMap<String, String> containing details of the track 3. ArrayList<Item> containing all playlists for a uid
+	 * Returns - void, populates the playlistScoreMap HashMap<String, ArrayList<Integer>>  
 	*/
-	public void generateScores(ArrayList<Integer> scores, ArrayList<Integer> tieBreakerScores, HashMap<String, String> trackDetailsMap, ArrayList<Item> playlistItems) {
+	public void generateScores(HashMap<String, ArrayList<Integer>> playlistScoreMap, HashMap<String, String> trackDetailsMap, ArrayList<Item> playlistItems) {
 		//Storing the track features into variables..
 		String trackGenre = trackDetailsMap.get("genre");
 		String trackArtist = trackDetailsMap.get("artist");
@@ -202,261 +162,40 @@ public class playlistRecommender implements RequestHandler<Object, String> {
 			int score = 0;
 			int tieBreakerScore = 0;
 			Item temp = playlistItems.get(i);
+			//Names of the playlist..
+			String playlistName = temp.get("playlist-name").toString();
 			//comparing priority 1 item..(GENRE)
-			score += getGenreScore(trackGenre, temp);
+			score += rputil.getGenreScore(trackGenre, temp);
 			//Comparing priority 2 item..(ERA)
-			score += getEraScore(trackYear, temp);
+			score += rputil.getEraScore(trackYear, temp);
 			//Comparing priority 3 item..(ARTIST)..
-			score += getArtistScore(trackArtist, temp);
+			score += rputil.getArtistScore(trackArtist, temp);
 			//Comparing priority 4 item..(ALBUM)..
-			score += getAlbumScore(trackAlbum, temp);
+			score += rputil.getAlbumScore(trackAlbum, temp);
 			//Comparing priority 5 item..(Last Played)
-			score += getLastPlayedScore(temp);
-			tieBreakerScore += getLastPlayedScore(temp);
+			score += rputil.getLastPlayedScore(temp);
+			tieBreakerScore += rputil.getLastPlayedScore(temp);
 			//Comparing priority 6 item..(No-of-tracks)..
 			score += Constants.NO_OF_TRACKS_CONSTANT * Integer.parseInt(temp.get("no-of-tracks").toString()); 
 			tieBreakerScore += Constants.NO_OF_TRACKS_CONSTANT * Integer.parseInt(temp.get("no-of-tracks").toString());
 			//Comparing priority 7 item..(Popularity)
-			score += getPopularityScore(trackPopularity, temp);
-			tieBreakerScore += getPopularityScore(trackPopularity, temp);
+			score += rputil.getPopularityScore(trackPopularity, temp);
+			tieBreakerScore += rputil.getPopularityScore(trackPopularity, temp);
 			//Comparing priority 8 item..(Duration)..
-			score += getDurationScore(temp);
-			tieBreakerScore += getDurationScore(temp);
+			score += rputil.getDurationScore(temp);
+			tieBreakerScore += rputil.getDurationScore(temp);
 			
 			myLog.logInfo("Total Score for playlist at index = "+i+" is = "+score);
 			myLog.logInfo("Total Tie Breaker Score for playlist at index = "+i+" is = "+tieBreakerScore);
-			scores.add(score);
-			tieBreakerScores.add(tieBreakerScore);
+			
+			//To temporarily store the score and tie score before putting into the HashMap...
+			ArrayList<Integer> tempStore = new ArrayList<Integer>();
+			tempStore.add(score);
+			tempStore.add(tieBreakerScore);
+			//Updating the map with the playlist name as key and list of score and tie scores..
+			playlistScoreMap.put(playlistName, tempStore);
 		}
 	}
-	/*
-	 *  Purpose - To get the playlist names and store into a given arraylist
-	 *  Arguments - 1. Empty ArrayList<String> 2. ArrayList<Item> containing all playlists
-	 *  Returns - void, populates the empty arraylist 
-	*/
-	public void getNames(ArrayList<String> names, ArrayList<Item> playlistItems) {
-		for(int i=0; i<playlistItems.size(); i++) {
-			Item temp = playlistItems.get(i);
-			String playlistName = temp.get("playlist-name").toString();
-			names.add(playlistName);
-		}
-	}
-	/*
-	 *  Purpose - To get the scores for genre matches
-	 *  Arguments - 1. String 2. Instance of an Item
-	 *  Returns - int - Score 
-	*/
-	public int getGenreScore(String trackGenre, Item temp) {
-		int score = 0;
-		String genre1 = temp.get("genre-rank1").toString();
-		String genre2 = temp.get("genre-rank2").toString();
-		String genre3 = temp.get("genre-rank3").toString();
-		String genre4 = temp.get("genre-rank4").toString();
-		String genre5 = temp.get("genre-rank5").toString();
-		
-		if(trackGenre.equals(genre1)) {
-			score += Constants.PRIORITY_1_CONSTANT * Constants.VALUE1;
-		}
-		else if(trackGenre.equals(genre2)) {
-			score += Constants.PRIORITY_1_CONSTANT * Constants.VALUE2;
-		}
-		else if(trackGenre.equals(genre3)) {
-			score += Constants.PRIORITY_1_CONSTANT * Constants.VALUE3;
-		}
-		else if(trackGenre.equals(genre4)) {
-			score += Constants.PRIORITY_1_CONSTANT * Constants.VALUE4;
-		}
-		else if(trackGenre.equals(genre5)) {
-			score += Constants.PRIORITY_1_CONSTANT * Constants.VALUE5;
-		}
-		
-		myLog.logInfo("Score of Genre Match is = "+score);
-		return score;
-	}
-	/*
-	 *  Purpose - To get the scores for Era matches
-	 *  Arguments - 1. int 2. Instance of an Item
-	 *  Returns - int - Score 
-	*/
-	public int getEraScore(int trackYear, Item temp) {
-		int score = 0;
-		double percentage = 0.0;
-		int era1 = Integer.parseInt(temp.get("era1").toString());
-		int era2 = Integer.parseInt(temp.get("era2").toString());
-		int era3 = Integer.parseInt(temp.get("era3").toString());
-		int era4 = Integer.parseInt(temp.get("era4").toString());
-		int era5 = Integer.parseInt(temp.get("era5").toString());
-		
-		int totalEra = era1+era2+era3+era4+era5;
-		
-		//To handle divide by zero error..
-		if(totalEra > 0) {
-			if(trackYear >= 1920 && trackYear <=1940) {
-				percentage = (double)era1/(double)totalEra; 
-			}
-			else if(trackYear > 1940 && trackYear <=1960) {
-				percentage = (double)era2/(double)totalEra;
-			}
-			else if(trackYear > 1960 && trackYear <=1980) {
-				percentage = (double)era3/(double)totalEra;
-			}
-			else if(trackYear > 1980 && trackYear <=2000) {
-				percentage = (double)era4/(double)totalEra;
-			}
-			else{
-				percentage = (double)era5/(double)totalEra;
-			}
-		}
-		else {
-			percentage = 0;
-		}
-		
-		score += Constants.PRIORITY_2_CONSTANT * percentage;
-
-		myLog.logInfo("Score of Era Match is = "+score);
-		return score;
-	}
-	/*
-	 *  Purpose - To get the scores for Artist matches
-	 *  Arguments - 1. String 2. Instance of an Item
-	 *  Returns - int - Score 
-	*/
-	public int getArtistScore(String trackArtist, Item temp) {
-		int score = 0;
-		String artist1 = temp.get("artist-rank1").toString();
-		String artist2 = temp.get("artist-rank2").toString();
-		String artist3 = temp.get("artist-rank3").toString();
-		String artist4 = temp.get("artist-rank4").toString();
-		String artist5 = temp.get("artist-rank5").toString();
-		
-		if(trackArtist.equals(artist1)) {
-			score += Constants.PRIORITY_3_CONSTANT * Constants.VALUE1;
-		}
-		else if(trackArtist.equals(artist2)) {
-			score += Constants.PRIORITY_3_CONSTANT * Constants.VALUE2;
-		}
-		else if(trackArtist.equals(artist3)) {
-			score += Constants.PRIORITY_3_CONSTANT * Constants.VALUE3;
-		}
-		else if(trackArtist.equals(artist4)) {
-			score += Constants.PRIORITY_3_CONSTANT * Constants.VALUE4;
-		}
-		else if(trackArtist.equals(artist5)) {
-			score += Constants.PRIORITY_3_CONSTANT * Constants.VALUE5;
-		}
-		
-		myLog.logInfo("Score of Artist Match is = "+score);
-		return score;
-	}
-	/*
-	 *  Purpose - To get the scores for Album matches
-	 *  Arguments - 1. String 2. Instance of an Item
-	 *  Returns - int - Score 
-	*/
-	public int getAlbumScore(String trackAlbum, Item temp) {
-		int score = 0;
-		String album1 = temp.get("album-rank1").toString();
-		String album2 = temp.get("album-rank2").toString();
-		String album3 = temp.get("album-rank3").toString();
-		String album4 = temp.get("album-rank4").toString();
-		String album5 = temp.get("album-rank5").toString();
-		
-		if(trackAlbum.equals(album1)) {
-			score += Constants.PRIORITY_4_CONSTANT * Constants.VALUE1;
-		}
-		else if(trackAlbum.equals(album2)) {
-			score += Constants.PRIORITY_4_CONSTANT * Constants.VALUE2;
-		}
-		else if(trackAlbum.equals(album3)) {
-			score += Constants.PRIORITY_4_CONSTANT * Constants.VALUE3;
-		}
-		else if(trackAlbum.equals(album4)) {
-			score += Constants.PRIORITY_4_CONSTANT * Constants.VALUE4;
-		}
-		else if(trackAlbum.equals(album5)) {
-			score += Constants.PRIORITY_4_CONSTANT * Constants.VALUE5;
-		}
-		
-		myLog.logInfo("Score of Album Match is = "+score);
-		return score;
-	}
-	/*
-	 *  Purpose - To get the scores for Last played
-	 *  Arguments - 1. Instance of an Item
-	 *  Returns - int - Score 
-	*/
-	public int getLastPlayedScore(Item temp) {
-		int score = 0;
-		int lastPlayedValue = 0;
-		//Handling the number format exception during parsing to int..
-		try {
-			if(Integer.parseInt(temp.get("last-played").toString()) != Constants.NEGATIVE_LAST_PLAYED) {
-				//The case in which the playlist is non - empty. When empty, value == -1 and should be alloted zero..
-				if(Integer.parseInt(temp.get("last-played").toString()) <= Constants.LAST_PLAYED_LOWER_THRESHOLD) {
-					lastPlayedValue = Constants.LAST_PLAYED_LOWER_POSITIVE;
-				}
-				else if(Integer.parseInt(temp.get("last-played").toString()) > Constants.LAST_PLAYED_LOWER_THRESHOLD && 
-						Integer.parseInt(temp.get("last-played").toString()) <= Constants.LAST_PLAYED_MIDDLE_THRESHOLD){
-					
-					lastPlayedValue = Constants.LAST_PLAYED_MIDDLE_POSITIVE;
-				}
-				else if(Integer.parseInt(temp.get("last-played").toString()) > Constants.LAST_PLAYED_MIDDLE_THRESHOLD && 
-						Integer.parseInt(temp.get("last-played").toString()) <= Constants.LAST_PLAYED_UPPER_THRESHOLD) {
-					
-					lastPlayedValue = Constants.LAST_PLAYED_UPPER_POSITIVE;
-				}
-				else {
-					lastPlayedValue = Constants.LAST_PLAYED_NEGATIVE;
-				}
-			}
-			else {
-				lastPlayedValue = 0;
-			}
-		}
-		catch(NumberFormatException e) {
-			myLog.logError("Number Format Exception occured = "+e);
-			return 0;
-		}
-		score += Constants.PRIORITY_5_CONSTANT * lastPlayedValue;
-		myLog.logInfo("Score of Last-Played Match is = "+score);
-		return score;
-	}
-	/*
-	 *  Purpose - To get the scores for Popularity proximity
-	 *  Arguments - 1. int 2. Instance of an Item
-	 *  Returns - int - Score 
-	*/
-	public int getPopularityScore(int trackPopularity, Item temp) {
-		int score = 0;
-		int popularity = Integer.parseInt(temp.get("popularity").toString());
-		//To avoid divide by zero and to ensure denominator is not less than 1..
-		if(popularity != 0) {
-			// If the playlist is not empty, it will have a non - zero popularity..
-			if(Math.abs(trackPopularity - popularity) > 1) {
-				score += (double)(1/(double)(Math.abs(trackPopularity - popularity))) * Constants.POPULARITY_CONSTANT;
-			}
-			else {
-				score += Constants.POPULARITY_CONSTANT;
-			}
-		}
-		else {
-			// For empty playlist, the popularity is equal to zero..
-			score = 0;
-		}
-		myLog.logInfo("Score of Popularity Match is = "+score);
-		return score;
-	}
-	/*
-	 *  Purpose - To get the scores for Duration proximity
-	 *  Arguments - 1. Instance of an Item
-	 *  Returns - int - Score 
-	*/
-	public int getDurationScore(Item temp) {
-		int score = 0;
-		int duration = temp.getInt("playlist-duration");
-		score += duration * Constants.DURATION_CONSTANT;
-		myLog.logInfo("Score of Duration Match is = "+score);
-		return score;
-	}
+	
 }
 //END OF CODE
