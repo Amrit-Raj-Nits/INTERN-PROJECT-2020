1f3d69715ab41530700a9bedfcba627efedc3044
Amrit Raj <amritrajnits@gmail.com>
2020-06-12 14:55:50 +0530

    Updated sort criteria

--- RecommendPlaylist/src/main/java/RecommendPlaylist/PlaylistSort.java
+++ RecommendPlaylist/src/main/java/RecommendPlaylist/PlaylistSort.java
@@ -1,36 +1,39 @@
-package RecommendPlaylist;
+package recommendplaylist;
 /*
-	This class implements comparator class so as to sort the ArrayList of Names of playlists with respect to its score
-	The Map has the playlist names as keys and scores->tieScores as values (arraylist). The sorting is done in descending 
-	order of the scores.
+This class implements comparator class so as to sort the ArrayList of Names of playlists with respect to its score
+The Map has the playlist names as keys and scores->tieScores as values (arraylist). The sorting is done in descending 
+order of the scores.
 */
 import java.io.*;
 import java.util.HashMap;
+import utils.recommendPlaylistLogKeeper;
 import java.util.Comparator;
 import java.util.ArrayList;
 public class PlaylistSort implements Comparator<String>{
+	
+	recommendPlaylistLogKeeper myLog = new recommendPlaylistLogKeeper();
+	
 	private HashMap<String, ArrayList<Integer>> frequency = new HashMap<String, ArrayList<Integer>>();
 	public PlaylistSort(HashMap<String, ArrayList<Integer>> arr) {
+		myLog.logInfo("Creating a Comparator Map and copying original map here for Sorting");;
 		frequency = arr;
 	}
 	public int compare(String s1, String s2) {
-		if(frequency.get(s1).get(0) < frequency.get(s2).get(0)) {
+		if(frequency.get(s1).get(Constants.SCORES_INDEX) < frequency.get(s2).get(Constants.SCORES_INDEX)) {
 			return 1;
 		}
-		else if(frequency.get(s1).get(0) > frequency.get(s2).get(0)) {
+		else if(frequency.get(s1).get(Constants.SCORES_INDEX) > frequency.get(s2).get(Constants.SCORES_INDEX)) {
 			return -1;
 		}
 		else {
-			//If two playlists have the same score, we need to break the tie..so we add the tieBreaker scores
-			//to the calculated scores and then re-compare.
-			if((frequency.get(s1).get(0) + frequency.get(s1).get(1)) < (frequency.get(s2).get(0) + frequency.get(s2).get(1))) {
+			//If two playlists have the same score, we need to break the tie..so we compare the tie scores..
+			if((frequency.get(s1).get(Constants.TIE_INDEX)) < (frequency.get(s2).get(Constants.TIE_INDEX))) {
 				return 1;
 			}
-			else if((frequency.get(s1).get(0) + frequency.get(s1).get(1)) > (frequency.get(s2).get(0) + frequency.get(s2).get(1))) {
+			else if((frequency.get(s1).get(Constants.TIE_INDEX)) > (frequency.get(s2).get(Constants.TIE_INDEX))) {
 				return -1;
 			}
 			else {
-				//If they are still the same, we declare them as equally likely..
 				return 0;
 			}
 		}
